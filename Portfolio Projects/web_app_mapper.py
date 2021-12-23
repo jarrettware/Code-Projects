7b890f1a23f603e3ebf8f810cb2c702f4fe27450
jarrettware <88641071+jarrettware@users.noreply.github.com>
2021-12-22 21:10:03 -0500

    Create web_app_mapper.py
    Scanner that can hunt for all files that are reachable on a remote target. This can root out leftover installation files, directories that should be protected and other valuable information.

---
+++ Portfolio Projects/web_app_mapper.py
@@ -0,0 +1,47 @@
+import Queue
+import threading
+import os
+import urllib.error
+import urllib.parse
+import urllib.request
+
+threads = 10
+target = "http://www.test.com"
+directory = " /Users/justin/Downloads/joomla-3.1.1"  #directory where web app is saved
+filters = [".jpg", ".gif", ".png", ".css"]       #List of file extensions your not interested in
+
+#Change directory and store located files
+os.chdir(directory)
+web_paths = queue.Queue()
+
+#Search through directory to see if files don't match filter and add to queue
+for r, d, f in os.walk("."):
+    for files in f:
+        remote_path = "%s%s" % (r, files)
+        if remote_path.startswith("."):
+            remote_path = remote_path[1:]
+        if os.path.splitext(files)[1] not in filters:
+            web_paths.put(remote_path)
+
+
+def test_remote():
+    #Grab path from queue and add to the target's base path
+    while not web_paths.empty():
+        path = web_paths.get()
+        url = "%s%s" % (target, path)
+        request = urllib.request.Request(url)
+    #If Successfully retrieved the files print HTTP status code and full file path
+        try:
+            response = urllib.request.urlopen(request)
+            print("[%d] => %s" % (response.code, path))
+            response.close()
+    #If file isn't found or protected handle error to continue loop
+        except urllib.error.HTTPError as error:
+            print("Failed %s" % error.code)
+            pass
+
+#Loop until queue is empty
+for i in range(threads):
+    print("Spawning thread: %d" % i)
+    t = threading.Thread(target=test_remote)
+    t.start()
